2174ab296383f6d0698e5697b50d05ce17fe971d
Michele Mostoller <MMostoller@att.net>
2024-06-05 15:34:59 -0400

    Syntax Sugar Exercise

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -2,9 +2,20 @@
 {
     public class Program
     {
-        static void Main(string[] args)
+   
+
+
+static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            var answer = 4;
+            var response = (answer < 9) 
+
+                ? $"{answer} is less than nine" 
+                : $"{answer} is greater than nine";
+
+            Console.WriteLine(response);
+
         }
     }
 }
